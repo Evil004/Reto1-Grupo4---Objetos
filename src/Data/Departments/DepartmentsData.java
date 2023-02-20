20eba50d1a78c8d94e8c8c7002824879b4ff152f
jt <alutab2939@ieselcaminas.org>
2023-02-20 19:27:24 +0100

    DataDepartments

---
+++ src/Data/Departments/DepartmentsData.java
@@ -0,0 +1,20 @@
+package Data.Departments;
+
+import java.io.File;
+import java.util.ArrayList;
+/**
+ * autor/es: JonathanTaban
+ * This class stores the data of the Departments.
+ */
+public class DepartmentsData {
+    private static ArrayList<Departments> departments = new ArrayList<>();
+
+    public DepartmentsData(ArrayList<Departments> departments){
+        this.departments = departments;
+    }
+
+    public DepartmentsData(){
+        this.departments = new ArrayList<>();
+    }
+
+}
