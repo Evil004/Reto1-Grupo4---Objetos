cef86b4b2884a1171da9fa562cde43e4189667a1
Jose Vicente Ebri <aluebr5363@ieselcaminas.org>
2023-02-20 19:37:15 +0100

    Jose

--- src/Data/Employees/EmployeesData.java
+++ src/Data/Employees/EmployeesData.java
@@ -3,9 +3,13 @@ package Data.Employees;
 import java.util.ArrayList;
 
 public class EmployeesData {
-    ArrayList<Employee> employees;
+    private static ArrayList<Employee> employees;
 
     public EmployeesData(ArrayList<Employee> employees) {
         this.employees = employees;
+    }
+
+    public static ArrayList<Employee> getEmployees(){
+        return employees;
     }
 }
