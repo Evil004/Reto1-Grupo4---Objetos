00ac6463a8271e113649ff7eb997087b3fa16098
OFM <alufer7328@ieselcaminas.org>
2023-02-24 19:39:41 +0100

    .

--- src/Data/QuoteGroup/QuoteGroupQuery.java
+++ src/Data/QuoteGroup/QuoteGroupQuery.java
@@ -6,20 +6,20 @@ import Data.QuoteGroup.*;
 import java.util.ArrayList;
 
 public class QuoteGroupQuery {
-    public static void SalaryCostQuoteGroup(int category) {
+    public static void SalaryCostQuoteGroup(int quoteGroup) {
         ArrayList<QuoteGroup> quoteGroups = QuoteGroupsData.getQuoteGroups();
         ArrayList<Employee> employees = EmployeesData.getEmployees();
 
-        for (QuoteGroup quoteGroup: quoteGroups) {
+        for (QuoteGroup i: quoteGroups) {
             int employeesByGroup = 0;
             for (Employee employee: employees) {
-                if (employee.getQuoteGroup() == quoteGroup.getId()) {
+                if (employee.getQuoteGroup() == i.getId()) {
                     employeesByGroup++;
                 }
             }
 
-            if (quoteGroup.getId() == category) {
-                System.out.println("El grupo de cotizacion " + quoteGroup.getId() + " tiene un coste salarial de " + quoteGroup.getSalaryBase() * employeesByGroup + "€");
+            if (i.getId() == quoteGroup) {
+                System.out.println("El grupo de cotizacion " + i.getId() + " tiene un coste salarial de " + i.getSalaryBase() * employeesByGroup + "€");
 
             }
         }
